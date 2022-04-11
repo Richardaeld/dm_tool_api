62b535b2a387d18840226acf9e3976630c058ab6
Richardaeld <Richardaeld@gmail.com>
2022-04-11 18:06:46 -0400

    added knex and sqlite3 to depenencies

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,13 @@ const router = express.Router();
 
 const users = [
     {
+        id: uuidv4(),
         name: "john",
         last: "smith",
         age: 25
     },
     {
+        id: uuidv4(),
         name: "Mary",
         last: "Doe",
         age: 28
@@ -27,10 +29,10 @@ router.post('/', (req, res) => {
     console.log('Post route reached');
 
     const user = req.body;
-
     const userId = uuidv4();
+    const userWithId = {...user, id: userId }
 
-    users.push(user)
+    users.push(userWithId)
 
     res.send(`You've added ${user.name} ${user.last} to database!`)
 });
