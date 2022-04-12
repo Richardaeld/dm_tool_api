8fe41d852b9cdf2584ed880c5ff24089ebdb55fb
Richardaeld <Richardaeld@gmail.com>
2022-04-12 00:48:32 -0400

    Added real button table info

---
+++ migrations/20220412041019_button-tables.js
@@ -0,0 +1,78 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    //narrowMiss
+    return knex.schema.createTable('narrowMissButtons', tbl => {
+        tbl.increments();
+        tbl.text('name', 50)
+            .notNullable();
+        tbl.text('objName', 50)
+            .notNullable();
+        tbl.boolean('open')
+            .defaultTo(false);
+    })
+
+    .createTable('wildMagicButtons', tbl => {
+        tbl.increments();
+        tbl.text('name', 50)
+            .notNullable();
+        tbl.text('objName', 50)
+            .notNullable();
+        tbl.boolean('open')
+            .defaultTo(false);
+    })
+
+    .createTable('magicItemButtons', tbl => {
+        tbl.increments();
+        tbl.text('name', 50)
+            .notNullable();
+        tbl.text('objName', 50)
+            .notNullable();
+        tbl.boolean('open')
+            .defaultTo(false);
+    })
+
+    .createTable('navButtons', tbl => {
+        tbl.increments();
+        tbl.text('name', 50)
+            .notNullable();
+        tbl.text('objName', 50)
+            .notNullable();
+        tbl.boolean('open')
+            .defaultTo(false);
+    })
+
+    .createTable('spellLevelButtons', tbl => {
+        tbl.increments();
+        tbl.text('name', 50)
+            .notNullable();
+        tbl.text('objName', 50)
+            .notNullable();
+        tbl.boolean('open')
+            .defaultTo(false)
+    })
+
+    .createTable('diceButtons', tbl => {
+        tbl.increments();
+        tbl.integer('value', 5)
+            .notNullable();
+        tbl.boolean('open')
+            .defaultTo(false);
+    })
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('narrowSuccessButtons')
+  .dropTableIfExists('wildMagicButtons')
+  .dropTableIfExists('magicItemButtons')
+  .dropTableIdExists('navButtons')
+  .dropTableIfExists('spellLevelButtons')
+  .dropTableIfExists('diceButtons');
+};
