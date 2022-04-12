e2773708c6c207a3b574eed93bffd5e41b70fecb
Richardaeld <Richardaeld@gmail.com>
2022-04-12 19:03:16 -0400

    updated knex to work with SQL join methods

--- knexfile.js
+++ knexfile.js
@@ -7,10 +7,15 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './dev.sqlite3'
     },
-    useNullAsDefault: true
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   },
 
   staging: {
