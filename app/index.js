356c8a9a28a2725c599193b4df8b258fa9236404
Richardaeld <Richardaeld@gmail.com>
2022-04-15 01:11:37 -0400

    Heroku file structure test

---
+++ app/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const server = require('../server')
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`server is running on: http://localhost:/${port}`)
+})
