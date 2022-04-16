e96c89011061ae5fb447c2cb7f1faa919b373e38
Richardaeld <Richardaeld@gmail.com>
2022-04-15 20:01:25 -0400

    Additional heroku test

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const server = express();
 
 const PORT = 5000;
 
+server.get('/', (req, res) => {
+    res.status(200).json({ message: "Welcome to DM Tool" })
+});
+
 server.listen(5000, () => {
     console.log(`I am here, on: http//localhost:${PORT}`)
-})
+});
