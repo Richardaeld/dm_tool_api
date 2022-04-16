288659b119da75df71aab0db45a2db6d9f78777a
Richardaeld <Richardaeld@gmail.com>
2022-04-15 22:33:43 -0400

    bad recovery

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const express = require('express');
+const server = express();
+
 require('dotenv').config();
-const server = require('./server')
+// const server = require('./server')
 
 const port = process.env.PORT;
+
+server.get('/', (req, res) => {
+    res.json({message : "Welcome to DM Tool's API!"});
+});
 
 server.listen(port, () => {
     console.log(`server is running on: http://localhost:/${port}`)
