12c05e680d964cb3880603d1a41523cef7143481
Richardaeld <Richardaeld@gmail.com>
2022-04-15 22:35:42 -0400

    heroku test

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-// const express = require('express');
-// const server = express();
+const express = require('express');
+const server = express();
 
-// var bodyParser = require('body-parser');
-// server.use(bodyParser.json({limit: '50mb'}));
-// server.use(bodyParser.urlencoded({limit: "50mb", extended:true, parameterLimit: 50000}));
+var bodyParser = require('body-parser');
+server.use(bodyParser.json({limit: '50mb'}));
+server.use(bodyParser.urlencoded({limit: "50mb", extended:true, parameterLimit: 50000}));
 
-// server.use(express.json());
+server.use(express.json());
 
-// const learningRouter = require('./Routes/learningRoutes')
-// const spellsRouter = require('./Routes/spellRoutes')
+const learningRouter = require('./Routes/learningRoutes')
+const spellsRouter = require('./Routes/spellRoutes')
 
-// server.get('/', (req, res) => {
-//     res.json({message : "Welcome to DM Tool's API!"});
-// });
+server.get('/', (req, res) => {
+    res.json({message : "Welcome to DM Tool's API!"});
+});
 
-// server.use('/api/lessons', learningRouter)
-// server.use('/api/spells', spellsRouter)
+server.use('/api/lessons', learningRouter)
+server.use('/api/spells', spellsRouter)
 
-// module.exports = server;
+module.exports = server;
