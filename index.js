c47231f864125ee7260fa033414a3bc81eb2982b
Richardaeld <Richardaeld@gmail.com>
2022-04-18 17:56:59 -0400

    Updated packages, file locations, in attempt tp track down heroku/express bug

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+
 require('dotenv').config();
-const server = require('./server')
+// const server = require('./server')
 
 const port = process.env.PORT;
 
+
+
+server.get('/', (req, res) => {
+    res.json({message : "Welcome to DM Tool's API!"});
+});
 
 server.listen(port, () => {
     console.log(`server is running on: http://localhost:/${port}`)
