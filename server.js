43361a7b8bc504be129b00e7e8ac891c035799f7
Richardaeld <Richardaeld@gmail.com>
2022-04-19 17:44:34 -0400

    Added ability to create cookie

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const session = require('express-session')
+
 const server = express();
+
+const sessionConfig = {
+    name: 'dm_tool', // Name of Cookie
+    secret: process.env.SECRET, // Secret tat makes cookie effective
+    cookie: {
+        maxAge: 1000 * 60 * 60, // Time span of cookie life
+        secure: process.env.COOKIESECURE, // Allows cookie to be valid when created on http(false) or https (true)
+        httpOnly: true // No access from JavaScript
+    },
+    resave: false, // Allows to be resaved each pass
+    saveUnititialized: SAVEUNITITIALIZED, // GDPR laws cookies cannot be saved without client conscient
+}
 
 var bodyParser = require('body-parser');
 server.use(bodyParser.json({limit: '50mb'}));
 server.use(bodyParser.urlencoded({limit: "50mb", extended:true, parameterLimit: 50000}));
 
 server.use(express.json());
+server.use(session(sessionConfig))
 
 const learningRouter = require('./routes/learningRoutes')
 const spellsRouter = require('./routes/spellRoutes')
