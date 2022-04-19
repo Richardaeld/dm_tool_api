12df0dec520d651a90b58c55e383b615bfe1ab4b
Richardaeld <Richardaeld@gmail.com>
2022-04-18 20:29:31 -0400

    Added a admin table with password

---
+++ migrations/20220418231456_admin.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('admin', tbl => {
+    tbl.increments();
+    tbl.text('username', 128)
+        .notNullable()
+        .unique()
+        .index();
+    tbl.text('password', 255)
+        .notNullable;
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('admin');
+};
