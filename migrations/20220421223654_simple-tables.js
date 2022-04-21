4526a5db18e188aa0fd13c4884b2009a39372bd6
Richardaeld <Richardaeld@gmail.com>
2022-04-21 19:38:46 -0400

    table correction

--- migrations/20220421223654_simple-tables.js
+++ migrations/20220421223654_simple-tables.js
@@ -1,11 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
 exports.up = function(knex) {
     return knex.schema.createTable('dice', tbl => {
         tbl.increments();
-        tbl.integer('value');
+        tbl.integer('value')
+            .notNullable;
     })
 };
 
 
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
 exports.down = function(knex) {
     return knex.schema.dropTableIfExists('dice')
 };
