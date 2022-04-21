4b7d60ee83bdd4feb4cf0598b99f992459bda2e0
Richardaeld <Richardaeld@gmail.com>
2022-04-21 17:29:40 -0400

    removed ununsed code

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,52 +4,6 @@ const db = require('../models/adminHelper');
 
 const router = express.Router();
 
-// router.post('/register', (req, res) => {
-//     const credentials = req.body;
-//     const {username, password } = credentials;
-
-//     if (!(username && password)) {
-//         return res.status(400).json({ message: "Please enter both Username and password"});
-//     }
-
-//     const hash = bcrypt.hashSync(credentials.password, 12);
-//     credentials.password = hash;
-
-//     db.addUser(credentials)
-//         .then((user) => {
-//             res.status(200).json(user);
-//         })
-//         .catch((error) => {
-//             if (error.errno == 2067) {
-//                 res.status(400).json({ message: "Username is already taken"});
-//             } else {
-//                 res.status(500).json(error);
-//             }
-//         });
-// });
-
-// router.post('/login', (req, res) => {
-//     const { username, password } = req.body;
-
-//     if (!(username && password)) {
-//         return res.status(400).json({ message: "Please enter both Username and password" })
-//     }
-
-//     db.findUserByUsername(username)
-//     .then((user) => {
-//         if (user && bcrypt.compareSync(password, user.password)) {
-//             // const token = generateToken(user);
-
-//             res.status(200).json({ message: `welcome ${user.username}!`});
-//         } else {
-//             res.status(401).json({ message: "Invalid credentials"});
-//         }
-//     })
-//     .catch((error) => {
-//         res.status(500).json("123",error);
-//     });
-// });
-
 router.get('/', (req, res) => {
     // db.findAllUsers()
     db.find()
