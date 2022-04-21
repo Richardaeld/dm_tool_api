2d647cf2e8303d055037969132b5c6bdfd02c08f
Richardaeld <Richardaeld@gmail.com>
2022-04-20 20:17:41 -0400

    Added dependencies including cors, helmet, and morgan

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const learningRouter = require('./routes/learningRoutes');
 const spellsRouter = require('./routes/spellRoutes');
@@ -6,7 +9,11 @@ const adminRouter = require('./routes/adminRoutes');
 const authRouter = require('./auth/authRoutes');
 const restricted = require('./auth/restrictedMiddleware')
 
+
 const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(logger('dev'))
 
 // Allow for larger than normal json submission
 var bodyParser = require('body-parser');
