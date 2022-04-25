0804bd4e78f687d07c2fe5293f7aca0a23795885
Richardaeld <Richardaeld@gmail.com>
2022-04-25 15:43:58 -0400

    Join table test

--- models/buttonHelper.js
+++ models/buttonHelper.js
@@ -30,10 +30,18 @@ function viewMainNav (id) {
     return db('main_nav_buttons').where({ id }).first();
 }
 
-function viewMainAllChildren (id) {
+function viewMainAllChildren (parent_foreign_key) {
     // return db('sub_nav_buttons').where({ parent_foreign_key: id })
     return db('main_nav_buttons')
     .join('sub_nav_buttons', 'main_nav_buttons.id', 'sub_nav_buttons.parent_foreign_key')
+    .select(
+        'man_nav_buttons.id',
+        'man_nav_buttons.name',
+        'sub_nav_buttons.id',
+        'sub_nav_buttons.name',
+        'sub_nav_buttons.parent_foreign_key'
+    )
+    .where({ parent_foreign_key })
 }
 
 // ----------------Sub Nav
