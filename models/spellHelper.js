bb2babfe6e44143dab77dd7ae76fcb8007a37e2a
Richardaeld <Richardaeld@gmail.com>
2022-04-29 20:16:49 -0400

    Removed unused code and updated addManySpells to accept new parsed JSON

--- models/spellHelper.js
+++ models/spellHelper.js
@@ -1,20 +1,9 @@
-// const knex = require('knex');
-// const config = require('../knexfile');
-// const db = knex(config.development);
-
 const db = require('../dbConfig');
 
 module.exports = {
     addManySpells,
-    // findSpells,
-    find,
-    findLevels,
-    findLevel,
-    // addManyLevels
-    addLevel,
-    findClasses,
-    addClasses
-    // updateManyToManySpells
+    findSpellsByLevel,
+    findSpellByName
 }
 // -----------------------------------DB calls for tags many to many
 
@@ -33,67 +22,11 @@ async function addClasses (classes) {
 return await db('player_class').insert(classes, ['id'])
 }
 
-// -----------------------------------DB calls for levels
-function findLevel (id) {
-    return db('level')
-    .where({id})
-    .first();
-}
-
-function findLevels () {
-    return db('level')
-}
-
-async function addLevel (level) {
-    const [id] = await db('level').insert(level)
-    return id
-}
-
-
 // -----------------------------------DB calls for Spells
 
 
-async function addManySpells (spells) {
-    const allClasses = db('player_class')
-    var allSpells = spells.map(spell => {
-
-        // Breaks up spell casting into two fields
-        if (spell.casting_time.includes(",")) {
-            var spellCastingArray = spell.casting_time.split(",", 2);
-            spell.casting_time = spellCastingArray[0]
-            spell.casting_time_reaction_condition = spellCastingArray[1]
-            console.log(spellCastingArray)
-        }
-
-        // Adds spells components into own fields and deletes original field
-        // spell.component_material = spell.components.material,
-        // spell.component_somatic = spell.components.somatic,
-        // spell.component_verbal = spell.components.verbal,
-        // spell.component_material_description = spell.components.materials_needed
-        delete spell.components
-
-
-        if(spell.level == 'cantrip') {
-            spell.level = 0
-        } else {
-            spell.level = parseInt(spell.level);
-        }
-
-        return {...spell}
-    })
-
-    // const [id] = await db('spells').insert(allSpells)
-
-    // return (
-    //     id,
-    //     db('spells')
-    //     .where({id})
-    //     .first()
-    // )
-
-    // return await db('spells').insert(allSpells, ['id', 'name'])
-    return await db('spells').insert(allSpells)
-
+async function addManySpells (allSpells) {
+    return await db('spells').insert(allSpells, ['id', 'name'])
 }
 
 
