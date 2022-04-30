420fb9be28bd313e5e7bdde883d72b6cb2e57e3d
Richardaeld <Richardaeld@gmail.com>
2022-04-29 20:39:18 -0400

    Added missing paths to router from added content to spell models

--- routes/spellRoutes.js
+++ routes/spellRoutes.js
@@ -14,6 +14,38 @@ router.get('/', (req, res) => {
     })
 });
 
+router.get('/spellsByLevel/:level', (req, res) => {
+    const level = req.params;
+
+    db.findSpellsByLevel(level)
+    .then(spells => {
+        if (spells) {
+            res.status(200).json({ spells })
+        } else {
+            res.status(404).json({ message: 'Record not found' })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: `Unable to preform operation: ${error}` })
+    });
+});
+
+router.get('/spellByName', (req, res) => {
+    const name = req.params;
+
+    db.findSpellByName(name)
+    .then(spell => {
+        if (spell) {
+            res.status(200).json({ spell })
+        } else {
+            res.status(404).json({ 'Record not found' })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: `Unable to preform operation: ${error}` })
+    })
+})
+
 // -----------------------------------routes for auth spells
 router.post('/addSpells', (req, res) => {
     db.addSpells(req.body)
